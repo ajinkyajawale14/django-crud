8b5ea5047679dff967cbc19291e2408688c33f05
Ajinkya Jawale <ajinkyajawale14499@gmail.com>
2022-11-09 23:56:12 +0530

    admin setup

--- core/models.py
+++ core/models.py
@@ -1,7 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User Must have a password")
+
+        user = self.model(
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_admin = False
+        user.is_staff = False
+        user.save(using = self.db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User Must have a password")
+
+        user = self.model(
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self.db)
+        return user
+
 class User(AbstractUser):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
@@ -12,3 +46,4 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = UserManager()
